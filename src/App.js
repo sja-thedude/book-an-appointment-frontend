1a4df2fc0669e703feb5123ff505fe0e71327988
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-21 18:13:16 +0300

    Add Files for setup and Create Components

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Reservations from './components/dashboard/Reservations';
 import DoctorList from './components/dashboard/DoctorList';
 import BookAppointment from './components/dashboard/BookAppointment';
 import DoctorDetail from './components/dashboard/DoctorDetail';
+import ErrorPage from './components/landingPage/ErrorPage';
+import './app.css';
+import DetailReservation from './components/dashboard/DetailReservation';
 
 const SidebarLayout = () => (
   <>
@@ -18,20 +21,22 @@ const SidebarLayout = () => (
 
 function App() {
   return (
-    <>
+    <div className="body-style">
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route exact path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/*" element={<ErrorPage />} />
         <Route element={<SidebarLayout />}>
           <Route path="/user/dashboard" element={<Home />} />
           <Route path="/user/reservation" element={<Reservations />} />
+          <Route path="/user/reservation/:id" element={<DetailReservation />} />
           <Route path="/user/doctors" element={<DoctorList />} />
           <Route path="/user/doctors/:id" element={<DoctorDetail />} />
           <Route path="/user/appointments" element={<BookAppointment />} />
         </Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,35 +1,84 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
-import { Link } from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaRegUser, FaUser } from 'react-icons/fa';
+import { HiOutlineMailOpen } from 'react-icons/hi';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import { hitAPIWithSigninDetails } from '../../redux/reducer/user';
 
-const Login = () => (
-  <section className="login">
-    <h3>Welcome back</h3>
-    <form action="">
-      <h4>Login</h4>
-      <div className="form-group">
-        <label htmlFor="Email1">Email address</label>
-        <input type="email" className="form-control" id="Email1" placeholder="Enter email" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="name">Full Name</label>
-        <input type="text" className="form-control" id="name" placeholder="Enter full name" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input type="password" className="form-control" id="password" placeholder="password" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-    <p>
-      New here?
-      {' '}
-      <Link to="/register" style={{ textDecoration: 'none' }}>
-        Register
-      </Link>
+import styles from '../scss/Login.module.scss';
 
-    </p>
-  </section>
-);
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.UserReducer);
+  const { loggedIn } = state;
+  const [signedInSuccess, setSignedInSuccess] = useState(loggedIn);
+  const [email, setEmail] = useState('');
+  const [password, setPassowrd] = useState('');
+  const loginUser = (e) => {
+    e.preventDefault();
+    if (email === '' || password === '') return;
+    const newUser = {
+      email,
+      password,
+    };
+    dispatch(hitAPIWithSigninDetails(newUser));
+    setEmail('');
+    setPassowrd('');
+  };
+
+  useEffect(() => {
+    if (loggedIn === 'in') {
+      navigate('/user/dashboard', { replace: true });
+    }
+    if (loggedIn === 'err') {
+      setSignedInSuccess(loggedIn);
+    }
+  }, [state]);
+  return (
+    <section className={styles['login-section']}>
+      <div className={styles.loginIcon}><FaUser /></div>
+      <h2>Welcome back</h2>
+
+      <form onSubmit={loginUser}>
+        <h4>Member Login</h4>
+        <div className={styles['form-group']}>
+          <span className={styles.icon}><HiOutlineMailOpen /></span>
+          <input
+            type="email"
+            className="form-control"
+            id="Email1"
+            placeholder="Useremail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+
+        <div className={styles['form-group']}>
+          <span className={styles.icon}><RiLockPasswordLine /></span>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            onChange={(e) => setPassowrd(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+      <p>
+        New here?
+        {' '}
+        <Link to="/register" style={{ textDecoration: 'none' }}>
+          Register
+        </Link>
+      </p>
+    </section>
+  );
+};
 
 export default Login;
